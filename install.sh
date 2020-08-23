cd72f0fd5a4818e55dc480d751632395f3adc6b2
Kyuho Lee <modulo641@gmail.com>
2020-08-23 13:06:28 +0900

    Update docker install scripts

--- install.sh
+++ install.sh
@@ -7,17 +7,10 @@ clone_repo() {
 }
 
 install_docker() {
-    sudo apt update
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-    sudo apt update
-    apt-cache policy docker-ce
-    sudo apt install docker-ce -y
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
 
-    sudo usermod -aG docker ${USER}
-
-    sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
     sudo apt-get autoremove
