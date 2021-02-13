e900b1728dda323d5e1414ab8f625efdc292fe1f
Kyuho Lee <modulo641@gmail.com>
2021-02-14 02:23:07 +0900

    🔧 Edit docker install script

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ clone_repo() {
 install_docker() {
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
+    sudo usermod -aG docker $USER
+
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 }
 
 open_ports() {
