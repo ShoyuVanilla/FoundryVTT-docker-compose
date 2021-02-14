86d84302f0f3e7a486a1501646b1fe089a93099e
Kyuho Lee <modulo641@gmail.com>
2021-02-14 17:14:14 +0900

    ✨ Add stop commands

--- foundry_https.sh
+++ foundry_https.sh
@@ -6,6 +6,10 @@ up() {
     docker-compose -f $basedir/docker-compose-https.yml -f $basedir/docker-compose-https.override.yml up -d
 }
 
+stop() {
+    docker-compose -f $basedir/docker-compose-https.yml -f $basedir/docker-compose-https.override.yml stop
+}
+
 down() {
     docker-compose -f $basedir/docker-compose-https.yml -f $basedir/docker-compose-https.override.yml down
 }
@@ -14,9 +18,12 @@ case "$1" in
     up)
         up
         ;;
+    stop)
+        stop
+        ;;
     down)
         down
         ;;
     *)
-        echo "Usage: foundry-https { up | down }"
+        echo "Usage: foundry-https { up | stop | down}"
 esac
